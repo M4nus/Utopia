66d81bbebe86fb38cc776888e88549398f4da71e
Paula Zachacz <paula.zachacz@onet.pl>
2019-05-21 16:16:36 +0200

    Antoni animation done

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour
-{                      
-    float _moveSpeed;    
+{
+    public Animator animator;
+
+    float _moveSpeed;
 
     void Start()
     {
-        _moveSpeed = 1f;
+        _moveSpeed = 0f;
     }
 
     void Update ()
-    {        
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) )
+    {
+        animator.SetFloat("Speed", Mathf.Abs(_moveSpeed));
+        _moveSpeed = 0f;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) )
         {
+            _moveSpeed = 1f;
             transform.eulerAngles = new Vector3(0, 0, 0); // Normal
             Move();
         }
         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) )
         {
+            _moveSpeed = 1f;
             transform.eulerAngles = new Vector3(0, 180, 0); // Flipped      
             Move();
         } 
