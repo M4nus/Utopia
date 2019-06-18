de987f16c02c76059dab65e81afecd9c723f2221
JKWrobel <bociek428@gmail.com>
2019-06-18 17:28:13 +0200

    Fixed Antoni Movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,22 +5,24 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     public Animator animator;
-
+    Rigidbody2D rigidbody2D;
     float _moveSpeed = 10;
     public float _targetMoveSpeed = 10;
 
     void Start()
     {
         //_moveSpeed = 0f;
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update ()
     {
         animator.SetFloat("Speed", Mathf.Abs(_moveSpeed));
         _moveSpeed = 0f;
+        rigidbody2D.velocity = Vector3.zero;
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) )
         {
-            _moveSpeed = _targetMoveSpeed;
+            _moveSpeed = -_targetMoveSpeed;
             transform.eulerAngles = new Vector3(0, 0, 0); // Normal
             Move();
         }
@@ -35,6 +37,7 @@ public class MovementController : MonoBehaviour
 
     void Move()
     {
-        gameObject.transform.Translate(new Vector2(-_moveSpeed * Time.deltaTime, 0));       
+        rigidbody2D.velocity = new Vector3(_moveSpeed, 0.0f, 0.0f);
+        //gameObject.transform.Translate(new Vector2(-_moveSpeed * Time.deltaTime, 0));       
     }
 }
